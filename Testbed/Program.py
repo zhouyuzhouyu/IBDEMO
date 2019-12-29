19d887c1a9cc782bec7ed67de02a3a3ce84e6652
zhouyu <524112470@qq.com>
2019-12-29 19:08:39 +0800

    init

--- Testbed/Program.py
+++ Testbed/Program.py
@@ -1930,7 +1930,7 @@ def main():
         if args.global_cancel:
             app.globalCancelOnly = True
         # ! [connect]
-        app.connect("127.0.0.1", args.port, clientId=101)
+        app.connect("127.0.0.1", args.port, clientId=0)
         # ! [connect]
         print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
                                                       app.twsConnectionTime()))
